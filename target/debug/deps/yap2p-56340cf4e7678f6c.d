5ce8e24ddf41d22d1700d3b65019599fab3808db
Scurrra <iscurrra@gmail.com>
2023-04-11 08:33:52 +0300

    Diffie-Hellman in separate mod

--- target/debug/deps/yap2p-56340cf4e7678f6c.d
+++
@@ -1,7 +0,0 @@
-/home/scurrra/Documents/dev/ya/target/debug/deps/yap2p-56340cf4e7678f6c.rmeta: yap2p/src/lib.rs
-
-/home/scurrra/Documents/dev/ya/target/debug/deps/libyap2p-56340cf4e7678f6c.rlib: yap2p/src/lib.rs
-
-/home/scurrra/Documents/dev/ya/target/debug/deps/yap2p-56340cf4e7678f6c.d: yap2p/src/lib.rs
-
-yap2p/src/lib.rs:
--- target/debug/libyap2p.d
+++
@@ -1,1 +0,0 @@
-/home/scurrra/Documents/dev/ya/target/debug/libyap2p.rlib: /home/scurrra/Documents/dev/ya/yap2p/src/lib.rs /home/scurrra/Documents/dev/ya/yap2p/src/peer/mod.rs
