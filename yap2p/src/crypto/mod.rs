e9a808fbaca05d71028f9dab45f7877bf455b319
Scurrra <iscurrra@gmail.com>
2023-05-23 21:36:53 +0300

    Function for chuncking message data into packets

--- yap2p/src/crypto/mod.rs
+++ yap2p/src/crypto/mod.rs
@@ -14,7 +14,7 @@ pub mod history;
 /// Arguments
 /// 
 /// * `data` --- data to be splitted into chunks length 16 
-pub(crate) fn chunk_data(data: impl AsRef<[u8]>) -> Vec<GenericArray<u8, U16>> {
+pub(crate) fn chunk_data_for_encryption(data: impl AsRef<[u8]>) -> Vec<GenericArray<u8, U16>> {
     let length = data.as_ref().len();
 
     // check if we can split data into equal pieces length 16
@@ -34,10 +34,40 @@ pub(crate) fn chunk_data(data: impl AsRef<[u8]>) -> Vec<GenericArray<u8, U16>> {
         // and than manually copy the remainder into [0u8; 16] to "cheaply" add zeroes at the end
         let mut last = GenericArray::from([0u8; 16]);
         {
-            let (left, _) = last.split_at_mut(length%16);
-            left.copy_from_slice(&data.as_ref()[(length-length%16)..])
+            let (left, right) = last.split_at_mut(length%16);
+            left.copy_from_slice(&data.as_ref()[(length-length%16)..]);
+            last[15] = right.len() as u8;
         }
         buf.push(last);
+
+        return buf;
+    }
+}
+
+/// Split data into chunks for packets
+/// 
+/// Arguments
+/// 
+/// * `data` --- data to be splitted into chunks
+/// * `piece_len` --- maximum length of pieces
+pub(crate) fn chunk_data_for_packet_split(data: impl AsRef<[u8]>, piece_len: usize) -> Vec<Vec<u8>> {
+    let length = data.as_ref().len();
+
+    // check if we can split data into equal pieces length 16
+    if length % piece_len == 0 {
+        return data.as_ref()
+            .chunks(piece_len)
+            .map(|c| Vec::from(c))
+            .collect::<Vec<Vec<u8>>>()
+    } else { 
+        // if we can not split data into equal pieces, we have to add zeroes in the end
+        // the way to have less copies is to split all data without remainder (data.len() % 1120)
+        let mut buf = data.as_ref()[..(piece_len*(length/piece_len))]
+            .chunks(piece_len)
+            .map(|c| Vec::from(c))
+            .collect::<Vec<Vec<u8>>>();
+        // and finally add remainder
+        buf.push(data.as_ref()[(length-length%piece_len)..].to_vec());
 
         return buf;
     }
