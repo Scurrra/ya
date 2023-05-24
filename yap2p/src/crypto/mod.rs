8c68df5ca7bd530114009bf2eff554667bf089eb
Scurrra <iscurrra@gmail.com>
2023-05-24 14:03:17 +0300

    Chuncker for packets now returns VecDeque

--- yap2p/src/crypto/mod.rs
+++ yap2p/src/crypto/mod.rs
@@ -3,6 +3,8 @@
 //! Module contains structures for [Diffie-Hellman](dh::DH) public
 //! key exchange, encription keys' storing, and messages' storing.
 
+use std::collections::VecDeque;
+
 use generic_array::{typenum::U16, GenericArray};
 
 pub mod dh;
@@ -50,7 +52,7 @@ pub(crate) fn chunk_data_for_encryption(data: impl AsRef<[u8]>) -> Vec<GenericAr
 /// 
 /// * `data` --- data to be splitted into chunks
 /// * `piece_len` --- maximum length of pieces
-pub(crate) fn chunk_data_for_packet_split(data: impl AsRef<[u8]>, piece_len: usize) -> Vec<Vec<u8>> {
+pub(crate) fn chunk_data_for_packet_split(data: impl AsRef<[u8]>, piece_len: usize) -> VecDeque<Vec<u8>> {
     let length = data.as_ref().len();
 
     // check if we can split data into equal pieces length 16
@@ -58,16 +60,16 @@ pub(crate) fn chunk_data_for_packet_split(data: impl AsRef<[u8]>, piece_len: usi
         return data.as_ref()
             .chunks(piece_len)
             .map(|c| Vec::from(c))
-            .collect::<Vec<Vec<u8>>>()
+            .collect::<VecDeque<Vec<u8>>>()
     } else { 
         // if we can not split data into equal pieces, we have to add zeroes in the end
         // the way to have less copies is to split all data without remainder (data.len() % 1120)
         let mut buf = data.as_ref()[..(piece_len*(length/piece_len))]
             .chunks(piece_len)
             .map(|c| Vec::from(c))
-            .collect::<Vec<Vec<u8>>>();
+            .collect::<VecDeque<Vec<u8>>>();
         // and finally add remainder
-        buf.push(data.as_ref()[(length-length%piece_len)..].to_vec());
+        buf.push_back(data.as_ref()[(length-length%piece_len)..].to_vec());
 
         return buf;
     }
